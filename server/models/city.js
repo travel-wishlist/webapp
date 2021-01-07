bbd8d24450c1299894e8bdfa588699155d3cfe72
gegewepeh <andres.jordan.s@gmail.com>
2021-01-07 18:22:49 +0700

    add association

--- server/models/city.js
+++ server/models/city.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      City.belongsTo (models.User, {foreignKey: 'UserId'})
     }
   };
   City.init({
--- server/models/user.js
+++ server/models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.belongsTo (models.City, {foreignKey: 'UserId'})
     }
   };
   User.init({
