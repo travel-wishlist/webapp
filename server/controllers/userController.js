642d3d80867a78eec8fc1f58fb88588865ae3d97
janakraman <hifzhana@gmail.com>
2021-01-08 11:23:40 +0700

    added userUpdate

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,24 @@
+const { User } = require("../models")
+
 class UserController {
+  static updateUser(req, res, next){
+    const {username, email, userCity} = req.body;
+
+    User.update({
+      username,
+      email,
+      userCity
+    }, {
+      where: {
+        id: req.user.id
+      },
+      returning: true
+    }).then(response => {
+      res.status(200).json(response);
+    }) .catch(err => {
+      res.send(err)
+    })
+  }
 
 }
 
