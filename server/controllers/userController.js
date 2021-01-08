85f3848c4d70053d85331f9417e919adf55ca089
janakraman <hifzhana@gmail.com>
2021-01-08 11:42:44 +0700

    fixed bug user update, give new access token after update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,25 +1,42 @@
-const { User } = require("../models")
+const { User } = require("../models");
+const { createToken } = require("../helpers/jwt");
 
 class UserController {
-  static updateUser(req, res, next){
-    const {username, email, userCity} = req.body;
+  static updateUser(req, res, next) {
+    const { username, email, userCity } = req.body;
 
-    User.update({
-      username,
-      email,
-      userCity
-    }, {
-      where: {
-        id: req.user.id
+    User.update(
+      {
+        username,
+        email,
+        userCity,
       },
-      returning: true
-    }).then(response => {
-      res.status(200).json(response);
-    }) .catch(err => {
-      res.send(err)
-    })
+      {
+        where: {
+          id: req.user.id,
+        },
+        returning: true,
+      }
+    )
+      .then((response) => {
+        if (response[0] === 1) {
+          req.user = {
+            id: response[1][0].id,
+            username: response[1][0].username,
+            email: response[1][0].email,
+            userCity: response[1][0].userCity,
+          };
+          console.log(req.user);
+          const access_token = createToken(req.user);
+          res.status(200).json({ access_token });
+        } else {
+          res.json(response);
+        }
+      })
+      .catch((err) => {
+        res.send(err);
+      });
   }
-
 }
 
-module.exports = UserController
+module.exports = UserController;
